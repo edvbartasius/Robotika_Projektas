1b9e3f0ac4a0c817a96b3496ec02d484cc7713c2
Nojus Sajauskas <nojus.sajauskas@gmail.com>
2025-11-20 19:23:22 +0200

    No movement by default

--- assets/robot_script.lua
+++ assets/robot_script.lua
@@ -1,58 +1,34 @@
-function speedChange_callback(ui,id,newVal)
-    speed=minMaxSpeed[1]+(minMaxSpeed[2]-minMaxSpeed[1])*newVal/100
-end
-
 function sysCall_init()
-    -- This is executed exactly once, the first time this script is executed
     sim=require('sim')
-    simUI=require('simUI')
-    bubbleRobBase=sim.getObject('..') -- this is bubbleRob's handle
-    leftMotor=sim.getObject("../LeftMotor") -- Handle of the left motor
-    rightMotor=sim.getObject("../RightMotor") -- Handle of the right motor
-    noseSensor=sim.getObject("../SensingNose") -- Handle of the proximity sensor
-    minMaxSpeed={50*math.pi/180,300*math.pi/180} -- Min and max speeds for each motor
-    backUntilTime=-1 -- Tells whether bubbleRob is in forward or backward mode
-    robotCollection=sim.createCollection(0)
-    sim.addItemToCollection(robotCollection,sim.handle_tree,bubbleRobBase,0)
-    distanceSegment=sim.addDrawingObject(sim.drawing_lines,4,0,-1,1,{0,1,0})
-    robotTrace=sim.addDrawingObject(sim.drawing_linestrip+sim.drawing_cyclic,2,0,-1,200,{1,1,0})
-    graph=sim.getObject('./graph')
-    distStream=sim.addGraphStream(graph,'bubbleRob clearance','m',0,{1,0,0})
-    -- Create the custom UI:
-        xml = '<ui title="'..sim.getObjectAlias(bubbleRobBase,1)..' speed" closeable="false" resizeable="false" activate="false">'..[[
-        <hslider minimum="0" maximum="100" onchange="speedChange_callback" id="1"/>
-        <label text="" style="* {margin-left: 300px;}"/>
-        </ui>
-        ]]
-    ui=simUI.create(xml)
-    speed=(minMaxSpeed[1]+minMaxSpeed[2])*0.5
-    simUI.setSliderValue(ui,1,100*(speed-minMaxSpeed[1])/(minMaxSpeed[2]-minMaxSpeed[1]))
-end
-
-function sysCall_sensing()
-    local result,distData=sim.checkDistance(robotCollection,sim.handle_all)
-    if result>0 then
-        sim.addDrawingObjectItem(distanceSegment,nil)
-        sim.addDrawingObjectItem(distanceSegment,distData)
-        sim.setGraphStreamValue(graph,distStream,distData[7])
-    end
-    local p=sim.getObjectPosition(bubbleRobBase)
-    sim.addDrawingObjectItem(robotTrace,p)
+    bubbleRobBase=sim.getObject('..') 
+    leftMotor=sim.getObject("../LeftMotor")
+    rightMotor=sim.getObject("../RightMotor")
+    noseSensor=sim.getObject("../SensingNose")
+    distancePrinted = false
+    
+    -- Set robot position to (1,1,0.1)
+    sim.setObjectPosition(bubbleRobBase, {1, 1, 0.1})
+    
+    -- Stop motors
+    sim.setJointTargetVelocity(leftMotor, 0)
+    sim.setJointTargetVelocity(rightMotor, 0)
+    
+    print("Robot positioned at (1,1,0.1)")
 end
 
 function sysCall_actuation()
-    result=sim.readProximitySensor(noseSensor) -- Read the proximity sensor
-    -- If we detected something, we set the backward mode:
-    if (result>0) then backUntilTime=sim.getSimulationTime()+4 end 
-
-    if (backUntilTime<sim.getSimulationTime()) then
-        -- When in forward mode, we simply move forward at the desired speed
-        sim.setJointTargetVelocity(leftMotor,speed)
-        sim.setJointTargetVelocity(rightMotor,speed)
-    else
-        -- When in backward mode, we simply backup in a curve at reduced speed
-        sim.setJointTargetVelocity(leftMotor,-speed/2)
-        sim.setJointTargetVelocity(rightMotor,-speed/8)
+    -- Keep motors stopped
+    sim.setJointTargetVelocity(leftMotor, 0)
+    sim.setJointTargetVelocity(rightMotor, 0)
+    
+    if not distancePrinted then
+        local result, distance = sim.readProximitySensor(noseSensor)
+        if result > 0 then
+            print("Distance to nearest wall: " .. string.format("%.3f", distance) .. " meters")
+        else
+            print("No obstacle detected within sensor range")
+        end
+        distancePrinted = true
     end
 end
 
